478ca7232078a8b331c5049588407cda4e14ea22
Imtiaj Sajin <100506477+Imtiaj-Sajin@users.noreply.github.com>
2022-10-22 03:24:59 +0600

    Add files via upload

---
+++ getch().cpp
@@ -0,0 +1,26 @@
+//getch() is to stop execution wait for a key press to start
+#include <bits/stdc++.h>
+using namespace std;
+#include<unistd.h>
+
+#include<conio.h>>
+
+int main() {
+
+int i=1;
+
+int x=0;
+while(!kbhit()){
+
+
+
+
+}
+    for(;;){
+            getch();
+
+i++;
+cout<<i;
+}
+}
+
